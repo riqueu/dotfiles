f43b50fde7cd1ce5d59978bb4de003b0f71d429e
henrique <riquecbeltrao@hotmail.com>
2025-01-30 17:41:32 -0300

    indentation + binds

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -6,7 +6,13 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number relativenumber")
 vim.g.mapleader = " "
+local opts = { noremap = true, silent = true }
 
 -- System Clipboard
-vim.keymap.set({'n', 'v'}, '<C-c>', '"+y', { noremap = true, silent = true })
-vim.keymap.set({'n', 'v'}, '<C-v>', '"+p', { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'v' }, '<C-c>', '"+y', opts)
+vim.keymap.set({ 'n', 'v' }, '<C-v>', '"+p', opts)
+
+-- Indentation Binds
+vim.keymap.set('n', '<S-Tab>', '<<', opts)
+vim.keymap.set('i', '<S-Tab>', '<C-\\><C-N><<<C-\\><C-N>^i')
+vim.keymap.set('n', '<Tab>', '>>', opts)
--- .config/wezterm/modules/mappings.lua
+++ .config/wezterm/modules/mappings.lua
@@ -2,87 +2,87 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 return {
-	leader = { key = "Space", mods = "SHIFT" },
+  leader = { key = "Space", mods = "SHIFT" },
 
-	keys = {
+  keys = {
     -- spawn and kill tab
-		{
-			key = "w",
-			mods = "CTRL",
-			action = act.CloseCurrentPane({ confirm = true }),
-		},
+    {
+		  key = "w",
+		  mods = "CTRL",
+		  action = act.CloseCurrentPane({ confirm = true }),
+    },
     {
       key = 't',
       mods = 'CTRL',
       action = act.SpawnTab 'CurrentPaneDomain',
     },
-		-- activate resize mode
-		{
-			key = "r",
-			mods = "LEADER",
-			action = act.ActivateKeyTable({
-				name = "resize_pane",
-				one_shot = false,
-			}),
-		},
+    -- activate resize mode
+    {
+      key = "r",
+      mods = "LEADER",
+      action = act.ActivateKeyTable({
+        name = "resize_pane",
+        one_shot = false,
+      }),
+    },
 
-		-- toggle fullscreen
-		{
-			key = 'F11',
-			mods = '',
-			action = wezterm.action.ToggleFullScreen,
-		},
+    -- toggle fullscreen
+    {
+      key = 'F11',
+      mods = '',
+      action = wezterm.action.ToggleFullScreen,
+    },
 
-		-- focus panes
-		{
-			key = "k",
-			mods = "LEADER",
-			action = act.ActivatePaneDirection("Left"),
-		},
-		{
-			key = "i",
-			mods = "LEADER",
-			action = act.ActivatePaneDirection("Right"),
-		},
-		{
-			key = "e",
-			mods = "LEADER",
-			action = act.ActivatePaneDirection("Up"),
-		},
-		{
-			key = "n",
-			mods = "LEADER",
-			action = act.ActivatePaneDirection("Down"),
-		},
+    -- focus panes
+    {
+      key = "k",
+      mods = "LEADER",
+      action = act.ActivatePaneDirection("Left"),
+    },
+    {
+      key = "i",
+      mods = "LEADER",
+      action = act.ActivatePaneDirection("Right"),
+    },
+    {
+      key = "e",
+      mods = "LEADER",
+      action = act.ActivatePaneDirection("Up"),
+    },
+    {
+      key = "n",
+      mods = "LEADER",
+      action = act.ActivatePaneDirection("Down"),
+    },
 
-		-- add new panes
-		{
-			key = "v",
-			mods = "LEADER",
-			action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			key = "h",
-			mods = "LEADER",
-			action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-		},
-	},
+    -- add new panes
+    {
+      key = "v",
+      mods = "LEADER",
+      action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+    },
+    {
+      key = "h",
+      mods = "LEADER",
+      action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+    },
+  },
 
-	key_tables = {
-		resize_pane = {
-			{ key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 5 }) },
-			{ key = "k", action = act.AdjustPaneSize({ "Left", 5 }) },
+  key_tables = {
+    resize_pane = {
+      { key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 5 }) },
+      { key = "k", action = act.AdjustPaneSize({ "Left", 5 }) },
 
-			{ key = "RightArrow", action = act.AdjustPaneSize({ "Right", 5 }) },
-			{ key = "i", action = act.AdjustPaneSize({ "Right", 5 }) },
+      { key = "RightArrow", action = act.AdjustPaneSize({ "Right", 5 }) },
+      { key = "i", action = act.AdjustPaneSize({ "Right", 5 }) },
 
-			{ key = "UpArrow", action = act.AdjustPaneSize({ "Up", 2 }) },
-			{ key = "e", action = act.AdjustPaneSize({ "Up", 2 }) },
+      { key = "UpArrow", action = act.AdjustPaneSize({ "Up", 2 }) },
+      { key = "e", action = act.AdjustPaneSize({ "Up", 2 }) },
 
-			{ key = "DownArrow", action = act.AdjustPaneSize({ "Down", 2 }) },
-			{ key = "n", action = act.AdjustPaneSize({ "Down", 2 }) },
+      { key = "DownArrow", action = act.AdjustPaneSize({ "Down", 2 }) },
+      { key = "n", action = act.AdjustPaneSize({ "Down", 2 }) },
 
-			{ key = "Escape", action = "PopKeyTable" },
-		},
-	},
+      { key = "Escape", action = "PopKeyTable" },
+    },
+  },
 }
