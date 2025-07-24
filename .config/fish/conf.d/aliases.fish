a3d50ec19fee83a007c45d85c828e5911e134292
rique <riquecbeltrao@hotmail.com>
2025-07-24 14:22:39 -0300

    swaync fullscreen fix

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -7,14 +7,6 @@ alias cl clear
 alias vi nvim
 
 alias mirror "wl-mirror eDP-1 & disown"
-alias phonewifi "scrcpy -e --no-audio"
-alias phone "scrcpy -d --no-audio"
-
-alias frontcamwifi "scrcpy --video-source=camera --video-codec=h265 --camera-size=1920x1080 --camera-facing=front --no-audio -e"
-alias frontcam "scrcpy --video-source=camera --video-codec=h265 --camera-size=1920x1080 --camera-facing=front --no-audio -d"
-
-alias backcamwifi "scrcpy --video-source=camera --video-codec=h265 --camera-size=1920x1080 --camera-facing=back --no-audio -e"
-alias backcam "scrcpy --video-source=camera --video-codec=h265 --camera-size=1920x1080 --camera-facing=back --no-audio -d"
 
 alias susp "systemctl suspend"
 
