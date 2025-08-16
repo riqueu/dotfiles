e03a1093287b9aac2764c5371449f2e19aca1275
rique <riquecbeltrao@hotmail.com>
2025-08-16 12:45:13 -0300

    file finder optimization

--- scripts/file-finder.sh
+++ scripts/file-finder.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# get desktop environment
+# get desktop environment (i use tofi with hyprland and fuzzel with niri)
 case "$XDG_CURRENT_DESKTOP" in
     "Hyprland")
-        # tofi command with desired sizing
         launcher_command="tofi --font-size=12 --width=50% --height=50%"
         ;;
     "niri" | *)
@@ -12,26 +11,16 @@ case "$XDG_CURRENT_DESKTOP" in
         ;;
 esac
 
-mapfile=$(mktemp)
-selfile=$(mktemp)
+# list of relative paths and pipe it directly to the launcher
+selected_path=$(find "$1" \( -type f -o -type d \) \
+  -printf '%P\n' | ${launcher_command})
 
-# Build "relative::full" map
-# The "$1" refers to the first argument passed to the script (e.g., "~/Documents")
-find "$1" \( -type f -o -type d \) \
-  -printf '/%P::%p\n' > "$mapfile"
+# exit if no file selected
+[[ -z $selected_path ]] && exit 1
 
-# Show only the relative paths using the selected launcher
-cut -d ':' -f1 "$mapfile" | \
-  ${launcher_command} > "$selfile"
+# get absolute path of the selected file/directory
+full_path=$(realpath "$1/$selected_path")
 
-selected=$(<"$selfile")
-[[ -z $selected ]] && { rm -f "$mapfile" "$selfile"; exit 1; }
-
-# Lookup full path
-full_path=$(grep -m1 -F "${selected}::" "$mapfile" | cut -d ':' -f3-)
-
-# Clean up temporary files
-rm -f "$mapfile" "$selfile"
-
+# open the selected file/directory with the default application or file manager
 thunar "$full_path" &
 # kitty "$full_path" &
